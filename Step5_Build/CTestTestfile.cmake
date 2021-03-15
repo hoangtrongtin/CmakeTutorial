5e83eb0a295780a7f23b4ba242ea0048af2be9c7
hoang trong tin <hoangttin288@gmail.com>
2021-03-15 08:11:33 -0700

    Step5

---
+++ Step5_Build/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial
+# Build directory: /home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Runs "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "25")
+set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;45;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Usage "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial")
+set_tests_properties(Usage PROPERTIES  PASS_REGULAR_EXPRESSION "Usage:.*number" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;48;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp4 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "4")
+set_tests_properties(Comp4 PROPERTIES  PASS_REGULAR_EXPRESSION "4 is 2" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;62;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp9 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "9")
+set_tests_properties(Comp9 PROPERTIES  PASS_REGULAR_EXPRESSION "9 is 3" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;63;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp5 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "5")
+set_tests_properties(Comp5 PROPERTIES  PASS_REGULAR_EXPRESSION "5 is 2.236" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;64;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp7 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "7")
+set_tests_properties(Comp7 PROPERTIES  PASS_REGULAR_EXPRESSION "7 is 2.645" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;65;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp25 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "25")
+set_tests_properties(Comp25 PROPERTIES  PASS_REGULAR_EXPRESSION "25 is 5" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;66;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp-25 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "-25")
+set_tests_properties(Comp-25 PROPERTIES  PASS_REGULAR_EXPRESSION "-25 is [-nan|nan|0]" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;67;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+add_test(Comp0.0001 "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/Step5_Build/Tutorial" "0.0001")
+set_tests_properties(Comp0.0001 PROPERTIES  PASS_REGULAR_EXPRESSION "0.0001 is 0.01" _BACKTRACE_TRIPLES "/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;55;add_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;68;do_test;/home/notyet/LG/CPP/CmakeTutorial/CmakeTutorial/CMakeLists.txt;0;")
+subdirs("MathFunctions")
